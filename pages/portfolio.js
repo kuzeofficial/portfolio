c539b7710745dce868d37f7068385e4dba79986c
Cristian Fonseca <cristiancomas@hotmail.com>
2021-07-08 22:24:53 -0700

    Changes public images to format webp

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -10,10 +10,10 @@ function Portfolio() {
                         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-6 text-black dark:text-white text-center mt-12 mb-0">Last Projects</h1>
                         <div class="flex flex-wrap overflow-hidden">
                             <div class="w-1/2 overflow-hidden sm:w-full md:w-full lg:w-1/2 xl:w-1/2">
-                                <Card image="/images/ui_food_delivery_app.jpg" title="Hello" description="My name is Cristian"/>
+                                <Card image="/images/ui_food_delivery_app.webp" title="Hello" description="My name is Cristian"/>
                             </div>
                             <div class="w-1/2 overflow-hidden sm:w-full md:w-full lg:w-1/2 xl:w-1/2">
-                                <Card image="/images/ui_food_delivery_app.jpg" title="Hello" description="My Name is Cristian"/>
+                                <Card image="/images/ui_food_delivery_app.webp" title="Hello" description="My Name is Cristian"/>
                             </div>
                         </div>
                     </div>
